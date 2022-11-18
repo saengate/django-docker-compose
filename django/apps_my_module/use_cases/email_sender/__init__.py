d8ea773870e16d78f8bbed6f140c58e542567261
SaenGate <saengate@gmail.com>
2022-11-18 14:17:23 -0300

    Fix base

--- django/apps_my_module/use_cases/email_sender/__init__.py
+++ django/apps_my_module/use_cases/email_sender/__init__.py
@@ -1,7 +1,3 @@
-from apps_my_module.use_cases.email_sender.password_recovery_email import PasswordRecoveryEmail
-from apps_my_module.use_cases.email_sender.user_confirmation_email import UserConfirmationEmail
-
-
 all = (
     'PasswordRecoveryEmail',
     'UserConfirmationEmail',
