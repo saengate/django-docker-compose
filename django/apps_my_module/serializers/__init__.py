115fb0017f7995f80c637005e78013a6b124239d
SaenGate <saengate@gmail.com>
2022-12-05 09:13:17 -0300

    Create Serializer Blog

--- django/apps_my_module/serializers/__init__.py
+++ django/apps_my_module/serializers/__init__.py
@@ -1,5 +1,8 @@
 from apps_my_module.serializers.custom_serializer import CustomSerializer
+from apps_my_module.serializers.blog_serializer import BlogSerializer
+
 
 __all__ = [
     'CustomSerializer',
+    'BlogSerializer',
 ]
