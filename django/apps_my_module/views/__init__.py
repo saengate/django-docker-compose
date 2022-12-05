bd39d829b1f31a32df292f902ecfc16e5bd94f7b
SaenGate <saengate@gmail.com>
2022-12-05 09:13:54 -0300

    Create Blog views

--- django/apps_my_module/views/__init__.py
+++ django/apps_my_module/views/__init__.py
@@ -1,9 +1,11 @@
 from apps_my_module.views.custom_views import CustomViews
 from apps_my_module.views.user_views import (CustomRefreshJSONWebTokenView,
                                              UserViewSet)
+from apps_my_module.views.blog_views import BlogViews
 
 __all__ = [
     'UserViewSet',
     'CustomRefreshJSONWebTokenView',
     'CustomViews',
+    'BlogViews',
 ]
