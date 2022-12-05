dfff4b05a8bd101fc772a6472f3afc0a97c38170
SaenGate <saengate@gmail.com>
2022-12-05 09:12:56 -0300

    Create Model Blog

--- django/apps_my_module/models/__init__.py
+++ django/apps_my_module/models/__init__.py
@@ -1,8 +1,10 @@
 from apps_my_module.models.user import User
 from apps_my_module.models.user import UserToken
+from apps_my_module.models.blog import Blog
 
 
 __all__ = [
     'UserToken',
     'User',
+    'Blog',
 ]
