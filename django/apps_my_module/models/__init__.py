f8fae04fa30a544be8b634655fc660b9552b3d6a
SaenGate <saengate@gmail.com>
2022-12-09 16:17:28 -0300

    Add models Entry and Author

--- django/apps_my_module/models/__init__.py
+++ django/apps_my_module/models/__init__.py
@@ -1,5 +1,6 @@
 from apps_my_module.models.author import Author
 from apps_my_module.models.blog import Blog
+from apps_my_module.models.entry import Entry
 from apps_my_module.models.user import (
     User,
     UserToken,
@@ -8,6 +9,7 @@ from apps_my_module.models.user import (
 __all__ = [
     'Author',
     'Blog',
+    'Entry',
     'UserToken',
     'User',
 ]
---
+++ django/apps_my_module/models/entry.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from apps_my_module.models.author import Author
+from apps_my_module.models.blog import Blog
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField()
+    number_of_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
