83e0af6cfd1b4ceb6e37a2ca46c9cdccb69f6b4d
SaenGate <saengate@gmail.com>
2022-12-09 16:18:19 -0300

    Add serializers Author

--- django/apps_my_module/serializers/__init__.py
+++ django/apps_my_module/serializers/__init__.py
@@ -1,10 +1,11 @@
 from apps_my_module.serializers.custom_serializer import CustomSerializer
 from apps_my_module.serializers.blog_serializer import BlogSerializer
+from apps_my_module.serializers.author_serializer import AuthorSerializer
 from apps_my_module.serializers.user_serializer import (
     UserModelSerializer, UserSignUpSerializer, jwt_user_login_serializer)
 
 __all__ = [
-    'CustomSerializer',
+    'AuthorSerializer',
     'BlogSerializer',
     'CustomSerializer',
     'UserSignUpSerializer',
