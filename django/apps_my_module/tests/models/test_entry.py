2536008c4035efefe0d60c4b775a7aefdc1e8660
SaenGate <saengate@gmail.com>
2022-12-09 16:19:37 -0300

    Test examples num queries entry model

---
+++ django/apps_my_module/tests/models/test_entry.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+
+from apps_my_module.tests.factories import EntryFactory
+from apps_my_module.models import Entry
+
+
+class EntryModel(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_add_entry_when_all_ok_then_return_instance(self):
+        instance = EntryFactory().create()
+        self.assertIsInstance(instance, Entry)
+
+        with self.assertNumQueries(0):
+            instance.authors.all()
+            instance.blog.name
+
+    def test_query_entry_when_filter_then_return_instance(self):
+        EntryFactory().create()
+        EntryFactory().create()
+        EntryFactory().create()
+
+        with self.assertNumQueries(4):
+            entries = Entry.objects.all()
+            for item in entries:
+                item.authors.all()
+                item.blog.name
+
+    def test_select_releated_when_filter_then_return_instance(self):
+        EntryFactory().create()
+        EntryFactory().create()
+        EntryFactory().create()
+
+        with self.assertNumQueries(2):
+            entries = Entry.objects.select_related(
+                'blog',
+            ).prefetch_related(
+                'authors',
+            ).all()
+            for item in entries:
+                item.authors.all()
+                item.blog.name
