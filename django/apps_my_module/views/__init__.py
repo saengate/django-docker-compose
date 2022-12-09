4d20b2d489e1976dc82e5fdabcaa2d3dd43581a4
SaenGate <saengate@gmail.com>
2022-12-09 13:23:09 -0300

    Fix Login users view

--- django/apps_my_module/views/__init__.py
+++ django/apps_my_module/views/__init__.py
@@ -1,12 +1,7 @@
-from apps_my_module.views.user_views import (
-    UserViewSet,
-    CustomRefreshJSONWebTokenView,
-)
-from apps_my_module.views.custom_views import hello_world
-
+from apps_my_module.views.user_views import (CustomRefreshJSONWebTokenView,
+                                             UserViewSet)
 
 __all__ = [
     'UserViewSet',
     'CustomRefreshJSONWebTokenView',
-    'hello_world',
 ]
