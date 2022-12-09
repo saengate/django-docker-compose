fe26f049eeb0502f8740aebd59dfbb97bcfc38b3
SaenGate <saengate@gmail.com>
2022-12-09 12:34:43 -0300

    Add author model

--- django/apps_my_module/models/__init__.py
+++ django/apps_my_module/models/__init__.py
@@ -1,10 +1,13 @@
-from apps_my_module.models.user import User
-from apps_my_module.models.user import UserToken
+from apps_my_module.models.author import Author
 from apps_my_module.models.blog import Blog
-
+from apps_my_module.models.user import (
+    User,
+    UserToken,
+)
 
 __all__ = [
+    'Author',
+    'Blog',
     'UserToken',
     'User',
-    'Blog',
 ]
