2b939c52e95736b5a24dc7e2b31470517070915c
SaenGate <saengate@gmail.com>
2022-12-16 17:54:35 -0300

    Fix tests assert equal dict

--- django/apps_my_module/tests/serializers/test_entry_serializer.py
+++ django/apps_my_module/tests/serializers/test_entry_serializer.py
@@ -1,13 +1,11 @@
 # from unittest.mock import MagicMock
 from django.test import TestCase
-from rest_framework.exceptions import ValidationError
 from freezegun import freeze_time
 
 from apps_my_module.serializers import (
     EntrySerializer,
     ReadEntrySerializer,
 )
-from apps_my_module.models import Entry
 from apps_my_module.tests.factories import (
     BlogFactory,
     AuthorFactory,
@@ -28,8 +26,8 @@ class TestEntrySerializer(TestCase):
             'blog': self.blog_instance.id,
             'headline': 'Blab bla',
             'body_text': 'Bla bla',
-            'pub_date': DateUtils.today(),
-            'mod_date': DateUtils.today(),
+            'pub_date': DateUtils.get_today_as_str(),
+            'mod_date': DateUtils.get_today_as_str(),
             'authors': [self.author_instance.id],
             'number_of_comments': 50,
             'number_of_pingbacks': 60,
@@ -51,51 +49,23 @@ class TestEntrySerializer(TestCase):
         self.blog_serializer.is_valid(raise_exception=True)
         instance_entry = self.blog_serializer.save()
 
-        self.data['blog'] = self.blog_factory.data
-        self.data['blog']['id'] = self.blog_instance.id
-        self.data['authors'] = [self.author_factory.data]
-        self.data['authors'][0]['id'] = self.blog_instance.id
+        expected = self.data
+
+        expected['blog'] = self.blog_factory.data
+        expected['blog']['id'] = self.blog_instance.id
+        expected['authors'] = [self.author_factory.data]
+        expected['authors'][0]['id'] = self.blog_instance.id
 
         with self.assertNumQueries(1):
             serializer = ReadEntrySerializer(
                 instance=instance_entry,
             )
-            serializer.data['authors'][0]['name']
-            serializer.data['blog']['name']
-            """  self.assertDictContainsSubset(
-                serializer.data,
-                self.data,
-            ) """
+            output = serializer.data
+            del output['id']
+            output['blog'] = dict(output['blog'])
+            output['authors'][0] = dict(output['authors'][0])
+            self.assertDictEqual(
+                output,
+                expected,
 
-    """ def test_is_valid_when_error_then_return_data(self):
-        del self.data['name']
-        with self.assertRaises(ValidationError):
-            self.assertFalse(self.serializer.is_valid(raise_exception=True))
-        self.assertIn('name', self.serializer.errors)
-
-    def test_save_when_ok_then_save_data(self):
-        self.assertTrue(self.serializer.is_valid(raise_exception=True))
-        instance = self.serializer.save()
-        blog = Blog.objects.get(id=instance.id)
-        self.assertIsInstance(blog, Blog)
-
-    def test_update_when_ok_then_save_data(self):
-        self.assertTrue(self.serializer.is_valid(raise_exception=True))
-        instance = self.serializer.save()
-        blog = Blog.objects.get(id=instance.id)
-        self.assertIsInstance(blog, Blog)
-
-        data = {
-            'name': 'fulanito',
-            'tagline': 'bbbbb bbb bb bb',
-        }
-
-        self.serializer = BlogSerializer(
-            data=data,
-            instance=instance,
-        )
-        self.assertTrue(self.serializer.is_valid(raise_exception=True))
-        instance = self.serializer.save()
-        blog = Blog.objects.get(id=instance.id)
-        self.assertEqual(blog.name, data['name'])
-        self.assertEqual(blog.tagline, data['tagline']) """
+            )
