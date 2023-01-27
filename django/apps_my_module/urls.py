d15ec5e739b4bb5a1d7a17a1fe21d6b9659b5de5
SaenGate <saengate@gmail.com>
2023-01-27 14:10:33 -0300

    Reverse merge class1

--- django/apps_my_module/urls.py
+++ django/apps_my_module/urls.py
@@ -9,7 +9,6 @@ from apps_my_module import views
 
 # Routers provide a way of automatically determining the URL conf.
 router = routers.SimpleRouter()
-router.register('custom/', views.CustomViews, basename='custom_views')
 
 urlpatterns = [
     path('', include(router.urls)),
