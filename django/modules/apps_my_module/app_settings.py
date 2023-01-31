d87d7a63a3d3bd699b542709266b2200c4ee88dd
SaenGate <saengate@gmail.com>
2023-01-31 14:38:28 -0300

    Fix model user

---
+++ django/modules/apps_my_module/app_settings.py
@@ -0,0 +1,2 @@
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
--- django/modules/users/models/user.py
+++ django/modules/users/models/user.py
@@ -1,26 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
-from modules.users.models.managers import CustomUserManager
 from rest_framework.authtoken.models import Token
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from modules.users.models.managers import CustomUserManager
 from modules.utils.date_utils import DateUtils
 
 
 class User(AbstractUser):
     verification_secret = models.CharField(
+        _('Verification secret'),
         max_length=256,
         blank=False,
         null=False,
     )
     is_confirmed = models.BooleanField(
+        _('Confirmed'),
         default=False,
     )
     wrong_password_count = models.SmallIntegerField(
+        _('Wrong password count'),
         default=0,
     )
 
-    feature_flags = models.JSONField(default=dict)
+    feature_flags = models.JSONField(
+        _('Feature flags'),
+        default=dict,
+    )
 
     objects = CustomUserManager()
 
@@ -28,6 +35,9 @@ class User(AbstractUser):
         self.last_login = DateUtils.now()
         self.wrong_password_count = 0
         self.save(update_fields=['last_login', 'wrong_password_count'])
+
+    def __str__(self) -> str:
+        return self.username
 
 
 class UserToken(Token):
